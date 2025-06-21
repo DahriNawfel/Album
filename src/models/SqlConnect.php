ce14c0bf9148519fc3778cec86f626e3296cfabf
DahriNawfel <nawfel.dahri14@gmail.com>
2025-06-21 13:45:50 +0200

    passage postgre

--- src/models/SqlConnect.php
+++ src/models/SqlConnect.php
@@ -6,25 +6,11 @@ use \PDO;
 
 class SqlConnect {
   public object $db;
-  private string $host;
-  private string $port;
-  private string $dbname;
-  private string $password;
-  private string $user;
 
   public function __construct() {
-    $this->host = getenv('DB_HOST');
-    $this->dbname = getenv('DB_NAME');
-    $this->user = getenv('DB_USER');
-    $this->password = getenv('DB_PASSWORD');
-    $this->port = getenv('DB_PORT');
+    $databaseUrl = getenv('DATABASE_URL');
 
-    $dsn = 'pgsql:host='.$this->host.';port='.$this->port.';dbname='.$this->dbname;
-    $this->db = new PDO(
-      $dsn,
-      $this->user,
-      $this->password
-    );
+    $this->db = new PDO($databaseUrl);
 
     $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $this->db->setAttribute(PDO::ATTR_PERSISTENT, false);
